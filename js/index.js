7ef1ee2bdf65f601061cb0beb2c7f373e00eecc8
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-06 17:02:54 -0500

    added photo swap effect using drag event listener

--- js/index.js
+++ js/index.js
@@ -21,10 +21,27 @@ for(i = 0; i < pics.length; i ++){
 }//for loop
 
 // wheel
-const page = document.querySelector('.main-navigation');
-console.log(page);
-page.addEventListener('wheel', (e) => {
-    page.style.backgroundColor = "yellow";
+const mainNav = document.querySelector('.main-navigation');
+console.log(mainNav);
+mainNav.addEventListener('wheel', (e) => {
+    mainNav.style.backgroundColor = "yellow";
 })
 
-//
+// select
+const funWords = document.querySelectorAll('p');
+for(i = 0; i < funWords.length; i++){
+    funWords[i].addEventListener('drag', (e) => {
+        e.target.textContent = "Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet?";
+    })
+}
+// drag (highlight text first, then drag)
+for(i = 0; i < pics.length; i ++){
+    pics[i].addEventListener('drag', (e) => {
+        e.target.setAttribute('src', "https://i.ytimg.com/vi/vDuDNCORd8I/maxresdefault.jpg")
+    })
+}
+
+// keydown
+
+const headers = document.querySelectorAll('h4');
+console.log(headers);
