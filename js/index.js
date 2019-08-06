79a8f8d12682f5e263632206a3d191bb0389ea65
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-06 13:56:35 -0500

    added for loop grayscale dblclick eventlistener

--- js/index.js
+++ js/index.js
@@ -1,9 +1,31 @@
-// Your code goes here
+// mouseover
 const logo = document.querySelector('.logo-heading');
 logo.addEventListener('mouseover', (e)=> {
     logo.textContent = "Hop On!";
 })
-
+// mouseout
 logo.addEventListener('mouseout', (e) => {
     logo.textContent = "Fun Bus";
 })
+
+// keydown
+
+
+// dblclick
+const pics = document.querySelectorAll('img');
+
+console.log(pics);
+    for(i = 0; i < pics.length; i ++){
+        pics[i].addEventListener('dblclick', (e) => {
+            e.target.style.filter = "grayscale(100%)";
+
+    })
+
+}
+
+
+
+    
+
+
+
