cca02d123af0104b980cbc6c04a90869ac4e51e2
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-06 13:14:57 -0500

    added mouseout to logo

--- js/index.js
+++ js/index.js
@@ -3,3 +3,7 @@ const logo = document.querySelector('.logo-heading');
 logo.addEventListener('mouseover', (e)=> {
     logo.textContent = "Hop On!";
 })
+
+logo.addEventListener('mouseout', (e) => {
+    logo.textContent = "Fun Bus";
+})
