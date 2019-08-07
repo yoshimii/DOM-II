a787abc01e88d0a617d42c216f70fd2ff14e7c5c
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-06 20:49:34 -0500

    added scroll and load event listeners

--- README.md
+++ README.md
@@ -35,20 +35,20 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 
 * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
 	* [x] `mouseover`
-	* [ ] `keydown`
+	* [x] `mouseout`
 	* [x] `wheel`
 	* [x] `drag / drop`
-	* [ ] `load`
+	* [x] `load`
 	* [ ] `focus`
 	* [ ] `resize`
-	* [ ] `scroll`
-	* [ ] `select`
+	* [x] `scroll`
+	* [x] `animationend`
 	* [x] `dblclick`
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
 * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
-* [ ] Stop the navigation from items from refreshing the page by using `preventDefault()`
+* [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
 
 ## Stretch Task:
 
--- js/index.js
+++ js/index.js
@@ -2,18 +2,17 @@
 const logo = document.querySelector('.logo-heading');
 logo.addEventListener('mouseover', (e)=> {
     logo.textContent = "Hop On!";
+    e.stopPropagation();
 })
+
 // mouseout
 logo.addEventListener('mouseout', (e) => {
     logo.textContent = "Fun Bus";
+    e.stopPropagation();
 })
-
-// keydown
-
 
 // dblclick
 const pics = document.querySelectorAll('img');
-
 for(i = 0; i < pics.length; i ++){
     pics[i].addEventListener('dblclick', (e) => {
         e.target.style.filter = "grayscale(100%)";
@@ -22,26 +21,65 @@ for(i = 0; i < pics.length; i ++){
 
 // wheel
 const mainNav = document.querySelector('.main-navigation');
-console.log(mainNav);
 mainNav.addEventListener('wheel', (e) => {
     mainNav.style.backgroundColor = "yellow";
+    e.stopPropagation();
 })
 
-// select
-const funWords = document.querySelectorAll('p');
-for(i = 0; i < funWords.length; i++){
-    funWords[i].addEventListener('drag', (e) => {
-        e.target.textContent = "Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet?";
-    })
-}
 // drag (highlight text first, then drag)
 for(i = 0; i < pics.length; i ++){
     pics[i].addEventListener('drag', (e) => {
         e.target.setAttribute('src', "https://i.ytimg.com/vi/vDuDNCORd8I/maxresdefault.jpg")
+        e.stopPropagation();
     })
 }
 
-// keydown
+//animationend
+const funWords = document.querySelectorAll('p');
+for(i = 0; i < funWords.length; i++){
+    funWords[i].addEventListener('animationend', (e) => {
+        e.target.style.color = "blue";
+        e.stopPropagation();
+    })
+}
 
-const headers = document.querySelectorAll('h4');
-console.log(headers);
+//scroll
+window.addEventListener('scroll', (e)=>{
+    for(i = 0; i < funWords.length; i++){
+        funWords[i].textContent = "Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet? Are we having fun yet?";
+        funWords[i].style.fontWeight = "bold";
+    }
+    e.stopPropagation();
+})
+
+// load
+let hyper = document.querySelector("html");
+window.addEventListener('load', (e)=> {
+    setTimeout(function(){
+        hyper.style.background = "url(https://img.buzzfeed.com/buzzfeed-static/static/2017-08/17/12/enhanced/buzzfeed-prod-fastlane-01/original-14351-1502988161-12.png)";
+    }, 3000);
+   
+    e.stopPropagation();
+})
+
+// // create new element
+// let boxx = document.createElement("div");
+// boxx.style.width = "50px";
+// boxx.style.height = "50px";
+// boxx.style.background = "yellow";
+
+// let bigBoxx = document.createElement("div");
+// bigBoxx.style.width = "300px";
+// bigBoxx.style.height = "300px";
+// bigBoxx.style.background = "red";
+// // add body to variable
+
+// const bod = document.querySelector("body");
+// // append to container
+// bigBoxx.append(boxx);
+// bod.append(bigBoxx);
+
+
+
+
+
