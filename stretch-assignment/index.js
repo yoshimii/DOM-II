26837b2857eadb511cad1d4e096f771c706e1b35
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-08-07 00:42:26 -0500

    block moves infinitely on click

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,28 +1,55 @@
-let nasa = document.querySelector(".blocks");
-let allBlocks = document.querySelectorAll(".block");
-console.log(allBlocks);
-let launchPad = Array.from(allBlocks);
-console.log(launchPad);
-for(i = 0; i < launchPad.length; i++){
+let nasa = document.querySelector(".blocks");// Stores parent node
+let allBlocks = document.querySelectorAll(".block"); // Stores child nodes (blocks)
+let launchPad = Array.from(allBlocks);// Converts child Nodelist to array
+for(i = 0; i < launchPad.length; i++){// Adds EventListeners to each child
     
-    launchPad[i].addEventListener('click', (e)=>{
+    launchPad[i].addEventListener('click', (e)=>{// On click removes target and prepends to parent
         let child = e.target;
         nasa.removeChild(child);
         nasa.prepend(child);
-        
+        e.stopPropagation();               
     });
-}
+
+    }
+
+    for(i = 0; i < launchPad.length; i++){// Adds EventListeners to each child
+            
+        launchPad[i].addEventListener('mousedown', (e)=>{// On click removes target and prepends to parent
+            let child = e.target;  
+            let pos = 0;            
+            child.style.position = "absolute";
+            child.style.left = "100px";
+   
+                let id = setInterval((frame) => {
+            if (pos == 1500) {
+              clearInterval(id);
+            } else {
+              pos++; 
+              
+              child.style.left = pos + "px"; 
+            }
+    }, 5);
+
+        });
+    
+        }
 
 
-// function moveRockets(i) {
-//     var pos = 0;
-//     var id = setInterval(frame, 5);
-//     function frame() {
-//       if (pos == 350) {
-//         clearInterval(id);
-//       } else {
-//         pos++; 
-//         i.style.bottom = pos + "px"; 
-//       }
-//     }
-//   }
+// let redItem = Array.from(red);
+// console.log(redItem);
+// let redBlock = document.querySelector(".block--red");
+// redBlock.style.color = "purple";
+// function moveRed(){
+//     let pos = 0;
+//     let id = setInterval((frame) => {
+//             if (pos == 350) {
+//               clearInterval(id);
+//             } else {
+//               pos++; 
+              
+//               red.style.left = pos + "px"; 
+//             }
+//     }, 5);
+// }
+
+// moveRed();
